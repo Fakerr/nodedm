0bcad81623cd89895896603484227fc029d3de53
walid <berrahal.walid07@gmail.com>
2015-07-09 14:06:46 +0200

    redirection case type fixed

--- public/services/auth.js
+++ public/services/auth.js
@@ -103,7 +103,10 @@ angular.module('MyApp')
                             type: 'material',
                             duration: 3
                         });
-                        $window.location.href = '/profile';
+                        if(!$rootScope.currentUser.type.localeCompare("client"))
+                            $window.location.href = '/profile';
+                        else if(!$rootScope.currentUser.type.localeCompare("annonceur"))
+                            $window.location.href = '/search';
                     })
                     .error(function () {
                         delete $window.localStorage.token;
